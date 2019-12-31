e8f6d682a9d8bc98ca2847aed11a0cb49f95fa98
Jayden Seric <me@jaydenseric.com>
2019-12-31 17:50:54 +1100

    Simplify the test index file.

--- test/index.js
+++ test/index.js
@@ -1,16 +1,12 @@
 'use strict'
 
 const { TestDirector } = require('test-director')
-const createTempDirTests = require('./createTempDir.test')
-const disposableDirectoryTests = require('./disposableDirectory.test')
-const fsPathExistsTests = require('./fsPathExists.test')
-const fsPathRemoveTests = require('./fsPathRemove.test')
 
 const tests = new TestDirector()
 
-fsPathExistsTests(tests)
-fsPathRemoveTests(tests)
-createTempDirTests(tests)
-disposableDirectoryTests(tests)
+require('./createTempDir.test')(tests)
+require('./disposableDirectory.test')(tests)
+require('./fsPathExists.test')(tests)
+require('./fsPathRemove.test')(tests)
 
 tests.run()
